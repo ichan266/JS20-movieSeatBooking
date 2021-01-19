7c46ccedaf29e77ae4b5f6d0d37f9ef5cdeb0bbd
Iris Kuhn <ichan266@hotmail.com>
2021-01-19 15:10:17 -0800

    script.js - save data to local storage in browser

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value; //*adding + (or add parseInt. in front will change the value to number from string *//
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelecedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-  
+
+  // Copy selected seats into an array
+  // Map through array
+  // return a new array indexes
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat)); //* ... inside an array is a spread operator, which copy elements in an array, in this case, everything in selectedSeats from line 11 into this array; seatsIndex is an array
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex)); //* localStorage is built into the browser; .setItem takes key-value pair. seatsIndex in an array so we need to wrap seatsIndex with JSON.stringify
+
   const selectedSeatsCount = selectedSeats.length;  //* get number of elements in an array or nodeList *//
   
   count.innerText = selectedSeatsCount;
@@ -18,7 +31,8 @@ function updateSelecedCount() {
 
 // Movie Select Event
 movieSelect.addEventListener('change', evt => {   //* because it is from a select list, you don't want click event *//
-  ticketPrice = +evt.target.value;
+  ticketPrice = +evt.target.value; //* the + sign change value from string to a number
+  setMovieData(evt.target.selectedIndex, evt.target.value);
   updateSelecedCount();
 }); 
 
